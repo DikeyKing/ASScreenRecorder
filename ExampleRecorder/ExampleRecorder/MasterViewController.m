6d2a28e32b8c0af716a272056249425cd39cc68d
dikeyking <dikeyking@gmail.com>
2017-07-17 16:12:23 +0800

    init commit

---
+++ ExampleRecorder/ExampleRecorder/MasterViewController.m
@@ -0,0 +1,126 @@
+//
+//  MasterViewController.m
+//  ExampleRecorder
+//
+//  Created by Alan Skipp on 23/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import "MasterViewController.h"
+#import "DetailViewController.h"
+#import "UIViewController+ScreenRecorder.h"
+#import <AudioToolbox/AudioToolbox.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface UIColor (Additions)
++ (UIColor *)randomColor;
+@end
+
+@implementation UIColor (Additions)
+
++ (UIColor *)randomColor
+{
+    return [UIColor colorWithRed:(arc4random() % 255/255.0f)
+                           green:(arc4random() % 255/255.0f)
+                            blue:(arc4random() % 255/255.0f)
+                           alpha:1.0];
+}
+@end
+
+
+@interface MasterViewController () {
+    NSMutableArray *_colors;
+}
+
+@property (nonatomic, strong) AVAudioPlayer *player;
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self prepareScreenRecorder];
+    
+    _colors = [NSMutableArray array];
+
+    for (NSUInteger i=0; i<20; i++) {
+        [_colors addObject:[UIColor randomColor]];
+    }
+    
+    [self playMusic];
+}
+
+- (void)playMusic
+{
+    if (!_player) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"nuannuan" ofType:@"mp3"];
+        NSError *error = nil;
+        NSURL *url = [NSURL fileURLWithPath:path];
+        _player = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    }
+    if (![_player isPlaying]) {
+//        [_player play];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+- (IBAction)insertNewObject:(id)sender
+{
+    [_colors insertObject:[UIColor randomColor] atIndex:0];
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
+    [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return _colors.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+    return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    cell.backgroundColor = _colors[indexPath.row];
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        [_colors removeObjectAtIndex:indexPath.row];
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showDetail"]) {
+        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        UIColor *color = _colors[indexPath.row];
+        [[segue destinationViewController] setColor:color];
+    }
+}
+
+@end
